bd25d9fe23ae4d53c4fecc844b8323c2391a3fae
SWE <weingartner@mwlaser.de>
2018-12-30 17:13:09 +0100

    SWE: SPS-Zyklus näher an 10ms

--- TestPrg_EVK1100/SWE_13_Arduino_04/GccApplication1/main.c
+++ TestPrg_EVK1100/SWE_13_Arduino_04/GccApplication1/main.c
@@ -14,7 +14,8 @@ void InitInterrupt(void)
 	TCCR0A = (1 << WGM01);					// CTC-Mode
 	TCCR0B = (1 << CS00) | (1 << CS02);		// Frequenz = 16MHz/1024/OCR0A
 	TIMSK0 = (1 << OCIE0A);					// Interrupt, wenn compare A ist
-	OCR0A = 200;							// Timer0 zählt von 0 bis OCR0A und löst dann einen Interrupt aus
+	OCR0A = 156;							// Timer0 zählt von 0 bis OCR0A und löst dann einen Interrupt aus
+											// 16MHz / 1024 / 156 = 100,16 Hz
 	
 	sei();	
 }
